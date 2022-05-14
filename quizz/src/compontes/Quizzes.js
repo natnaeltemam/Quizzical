8563673e9ab58c52709fedbefe82928f2ba2cdb2
naty alem <natnaeltemam1@gmail.com>
2022-05-14 07:37:35 +0300

    Change the background image of the quizz page and some style change and add some qution

--- quizz/src/compontes/Quizzes.js
+++ quizz/src/compontes/Quizzes.js
@@ -23,36 +23,40 @@ function About() {
 
             </div>
             <div className="que">
-                <h2 className='choseq'> How would one say goodbye in Spanish? </h2>
+                <h2 className='choseq'> What is the hottest planet in our Solar System? </h2>
                 <div className="choose">
-                    <button className="ch">Adiós</button>
-                    <button className="ch">Hola</button>
-                    <button className="ch">Au Revoir</button>
-                    <button className="ch">Salir</button>
+                    <button className="ch">Mercury</button>
+                    <button className="ch">Venus</button>
+                    <button className="ch">Mars</button>
+                    <button className="ch">Saturn</button>
                 </div>
             </div>
             <div className="que">
-                <h2 className='choseq'> How would one say goodbye in Spanish? </h2>
+                <h2 className='choseq'> In which country was the caesar salad invented? </h2>
                 <div className="choose">
-                    <button className="ch">Adiós</button>
-                    <button className="ch">Hola</button>
-                    <button className="ch">Au Revoir</button>
-                    <button className="ch">Salir</button>
+                    <button className="ch">Italy</button>
+                    <button className="ch">Portugal</button>
+                    <button className="ch">Mexico</button>
+                    <button className="ch">France</button>
                 </div>
 
             </div>
             <div className="que">
-                <h2 className='choseq'> How would one say goodbye in Spanish? </h2>
+                <div className="quezz">
+                    <h2 className='choseq'> How Many Hearts Does An Octopus Have? </h2>
+                </div>
                 <div className="choose">
-                    <button className="ch">Adiós</button>
-                    <button className="ch">Hola</button>
-                    <button className="ch">Au Revoir</button>
-                    <button className="ch">Salir</button>
+                    <button className="ch">One</button>
+                    <button className="ch">Two</button>
+                    <button className="ch">Three</button>
+                    <button className="ch">Four</button>
                 </div>
 
             </div>
 
 
+
+            <button> Check answers </button>
 
         </div>
     )
